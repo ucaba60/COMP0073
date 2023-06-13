e36a4d07196261c32ec94bf41be076fe1a0d3d66
ucaba60 <ucaba60@ucl.ac.uk>
2023-06-13 19:29:49 +0100

    Counts POS and Preplexity using BERT

---
+++ DetectGPT_Code/Modified_DetectGPT.py
@@ -0,0 +1,78 @@
+# Attempt at adopting DetectGPTs data pre-processing
+# Includes extracting Q&A from PubMed, and Prompt&Text from WPs
+# After that creates a dictionary of the original data
+# And extracts the Qs and Prompts, uses them to prompt GPT-3.5 to create text with max length
+# Save results, GPT-Text with different temperatures
+
+
+# First, data pre-processing, simple start with PREMED
+import random
+import datasets
+import spacy
+from collections import Counter
+from sklearn import metrics
+import spacy
+from collections import Counter
+import torch
+from transformers import BertTokenizer, BertForMaskedLM
+import spacy
+from collections import Counter
+
+nlp = spacy.load('en_core_web_sm')
+
+model = BertForMaskedLM.from_pretrained('bert-base-uncased')
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+def calculate_perplexity(text):
+    inputs = tokenizer(text, return_tensors='pt')
+
+    with torch.no_grad():
+        outputs = model(**inputs, labels=inputs['input_ids'])
+        loss = outputs.loss
+    return torch.exp(loss).item()
+
+def load_pubmed():
+    data = datasets.load_dataset('pubmed_qa', 'pqa_labeled', split='train')
+
+    # combine question and long_answer and add label, and create a spaCy Doc for each text
+    data = [(nlp(f'Question: {q} Answer: {a}'), 0) for q, a in zip(data['question'], data['long_answer'])]
+
+    return data
+
+data = load_pubmed()
+
+# Initialize counters
+pos_counts = Counter()
+function_word_counts = Counter()
+punctuation_counts = Counter()
+uppercase_counts = Counter()
+
+# Initialize variables for sentence length calculation
+total_tokens = 0
+total_sentences = 0
+
+# Initialize list to store each text's perplexity
+perplexities = []
+
+# Iterate over all tokens in the data only once
+for doc, _ in data:
+    text = doc.text
+    perplexities.append(calculate_perplexity(text))
+    for token in doc:
+        pos_counts[token.pos_] += 1
+        if token.text in {'a', 'in', 'of'}:
+            function_word_counts[token.text] += 1
+        if token.is_punct:
+            punctuation_counts[token.text] += 1
+        if token.text.isupper():
+            uppercase_counts[token.text] += 1
+    total_tokens += len(doc)
+    total_sentences += len(list(doc.sents))
+
+# Calculate average sentence length
+avg_sentence_length = total_tokens / total_sentences if total_sentences > 0 else 0
+
+# Calculate average perplexity
+avg_perplexity = sum(perplexities) / len(perplexities) if perplexities else 0
+
+
