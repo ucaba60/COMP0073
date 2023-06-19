a7aac83faa012b3930e0bee6c9d67b1f2697a1e6
ucaba60 <ucaba60@ucl.ac.uk>
2023-06-19 10:00:12 +0100

    VERSION 2 - WORKS WITH PUBMED/WRITINGPROMTS ADDING DAILYMAIL

---
+++ Logistic_Regression/Version_0.2/data_preprocessing_v0.2.py
@@ -0,0 +1,131 @@
+import datasets
+import re
+import pandas as pd
+import os
+
+DATASETS = ['pubmed_qa', 'writingprompts']
+DATA_PATH = 'data/writingPrompts'
+NUM_EXAMPLES = 150
+TAGS = ['[ WP ]', '[ OT ]', '[ IP ]', '[ HP ]', '[ TT ]', '[ Punch ]', '[ FF ]', '[ CW ]', '[ EU ]']
+
+
+def strip_newlines(text):
+    """
+    Removes newline characters from a string.
+    """
+    return ' '.join(text.split())
+
+
+def process_text(text, replacements):
+    """
+    Performs a series of replacements in a string.
+    """
+    for old, new in replacements.items():
+        text = text.replace(old, new)
+    return text
+
+
+def remove_whitespace_before_punctuations(text):
+    """
+    Removes whitespace before punctuation marks in a string.
+    """
+    return re.sub(r'\s([?.!,:;](?:\s|$))', r'\1', text)
+
+
+def load_pubmed(num_examples=NUM_EXAMPLES):
+    """
+    Loads the PubMed QA dataset.
+    """
+    data = datasets.load_dataset('pubmed_qa', 'pqa_labeled', split=f'train[:{num_examples}]')
+    data = [(f'Question: {q} Answer: {a}', 0) for q, a in zip(data['question'], data['long_answer'])]
+    return data
+
+
+def load_writingPrompts_dataset(data_path=DATA_PATH, num_examples=NUM_EXAMPLES):
+    """
+    Loads the WritingPrompts dataset.
+    """
+    with open(f'{data_path}/valid.wp_source', 'r', encoding='utf-8') as f:
+        prompts = f.readlines()[:num_examples]
+    with open(f'{data_path}/valid.wp_target', 'r', encoding='utf-8') as f:
+        stories = f.readlines()[:num_examples]
+
+    prompt_replacements = {tag: '' for tag in TAGS}
+    prompts = [process_text(prompt, prompt_replacements) for prompt in prompts]
+    prompts = [remove_whitespace_before_punctuations(prompt) for prompt in prompts]
+
+    story_replacements = {
+        ' ,': ',',
+        ' .': '.',
+        ' ?': '?',
+        ' !': '!',
+        ' ;': ';',
+        ' \'': '\'',
+        ' ’ ': '\'',
+        ' :': ':',
+        '<newline>': '\n',
+        '`` ': '"',
+        ' \'\'': '"',
+        '\'\'': '"',
+        '.. ': '... ',
+        ' )': ')',
+        '( ': '(',
+        ' n\'t': 'n\'t',
+        ' i ': ' I ',
+        ' i\'': ' I\'',
+        '\\\'': '\'',
+        '\n ': '\n',
+    }
+    stories = [process_text(story, story_replacements).strip() for story in stories]
+    joined = ["Prompt:" + prompt + " Story: " + story for prompt, story in zip(prompts, stories)]
+    filtered = [story for story in joined if 'nsfw' not in story.lower()]
+    data = [(story, 0) for story in filtered]
+    return data
+
+
+def load_data(dataset_name):
+    """
+    Loads a dataset based on its name.
+    """
+    if dataset_name == 'pubmed_qa':
+        return load_pubmed()
+    elif dataset_name == 'writingprompts':
+        return load_writingPrompts_dataset()
+    else:
+        raise ValueError(f"Dataset name {dataset_name} not recognized.")
+
+
+def preprocess_data(dataset):
+    """
+    Preprocesses a dataset.
+    """
+    if dataset not in DATASETS:
+        raise ValueError(f"Dataset name {dataset} not recognized.")
+
+    data = load_data(dataset)
+    data = list(dict.fromkeys(data))
+    data = [(strip_newlines(q).strip(), a) for q, a in data]
+    if dataset == 'pubmed_qa':
+        print(f"Loaded and pre-processed {len(data)} questions from the dataset")  # debug print
+
+    if dataset == 'writingprompts':
+        long_data = [(x, y) for x, y in data if len(x.split()) > 250]
+        if len(long_data) > 0:
+            data = long_data
+        print(f"Loaded and pre-processed {len(data)} prompts/stories from the dataset")  # debug print
+    return data
+
+
+def convert_to_csv(data, dataset_name, directory='Labelled_Data'):
+    """
+    Converts the data to a DataFrame and saves it to a CSV file in the specified directory.
+    """
+    # Check if directory exists, if not, create it
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Convert data to DataFrame
+    df = pd.DataFrame(data, columns=['text', 'label'])
+
+    # Write DataFrame to CSVv
+    df.to_csv(f'{directory}/{dataset_name}_Human_data.csv', index=False)
