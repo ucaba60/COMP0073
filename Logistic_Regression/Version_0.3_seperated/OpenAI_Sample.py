7e6f9391c10141f8f7bb8e2b2be94982f5c944de
ucaba60 <ucaba60@ucl.ac.uk>
2023-06-22 17:00:18 +0100

    SAMPLE T1 -COMPLETE

--- Logistic_Regression/Version_0.3_seperated/OpenAI_Sample.py
+++ Logistic_Regression/Version_0.3_seperated/OpenAI_Sample.py
@@ -1,37 +1,75 @@
-import openai
-import csv
 import pandas as pd
+import openai
+import time
+import csv
+import os
 
-# OpenAI API key
-openai.api_key = 'YOUR_OPEN_AI_KEY'
+BATCH_SIZE = 5  # Define the batch size
+openai.api_key = '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'
 
 
-def gpt3_chat_prompts():
-    # Load prompts from CSV file
-    df = pd.read_csv('prompts.csv')
+def generate_gpt_responses(prompt_csv_path, response_csv_path, model="gpt-3.5-turbo", temperature=1):
+    """
+    Generate GPT-3 responses for a list of prompts saved in a csv file.
+
+    Args:
+        prompt_csv_path (str): Path to the csv file containing the prompts.
+        response_csv_path (str): Path to the csv file where the responses will be saved.
+        model (str, optional): The ID of the model to use. Defaults to "gpt-3.5-turbo".
+        temperature (float, optional): Determines the randomness of the AI's output. Defaults to 1, as per OpenAI docs.
+
+    Returns:
+        None, generates a csv file with the responses.
+    """
+
+    # Load the prompts
+    df = pd.read_csv(prompt_csv_path)
     prompts = df['text'].tolist()
 
-    # Prepare data for new CSV file
-    data = []
+    # Initialize the starting point
+    start = 0
 
-    # For each prompt, send it to the GPT-3 model and get a response
-    for prompt in prompts:
-        message = {
-            "role": "system",
-            "content": "You are a helpful assistant."
-        },
-        {
-            "role": "user",
-            "content": prompt
-        }
-        response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=message)
-        data.append((prompt, response['choices'][0]['message']['content']))
+    # Check if the response file already exists
+    if os.path.exists(response_csv_path):
+        # If so, get the number of completed prompts from the file
+        with open(response_csv_path, "r", newline="", encoding='utf-8') as file:
+            start = sum(1 for row in csv.reader(file)) - 1  # Subtract 1 for the header
 
-    # Save responses to a new CSV file
-    with open('responses.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["Prompt", "Response"])
-        writer.writerows(data)
+    # Process the remaining prompts in batches
+    for i in range(start, len(prompts), BATCH_SIZE):
+        batch = prompts[i:i + BATCH_SIZE]
+        responses = []
+
+        for prompt in batch:
+            # Generate the response
+            response = openai.ChatCompletion.create(
+                model=model,
+                messages=[
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": prompt}
+                ],
+                temperature=temperature,
+            )
+
+            # Append the response to the list
+            responses.append('<<RESP>> ' + response['choices'][0]['message']['content'].strip())
+
+        # Save the responses to a new DataFrame
+        response_df = pd.DataFrame({
+            'Prompt': batch,
+            'Response': responses
+        })
+
+        # Write the DataFrame to the CSV file, appending if it already exists
+        if os.path.exists(response_csv_path):
+            response_df.to_csv(response_csv_path, mode='a', header=False, index=False)
+        else:
+            response_df.to_csv(response_csv_path, mode='w', index=False)
+
+        print(f"Batch {i // BATCH_SIZE + 1} completed")
 
 
-gpt3_chat_prompts()
+
+
+
+# generate_gpt_responses("Labelled_Data/prompts.csv", "Labelled_Data/t1_responses.csv")
