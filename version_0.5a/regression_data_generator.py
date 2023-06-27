ac5c33b90f05677eb72455de84e609c6ab9feaf3
ucaba60 <ucaba60@ucl.ac.uk>
2023-06-27 12:24:24 +0100

    EVERYTING WORKING -ADDED COUNTER

--- version_0.5a/regression_data_generator.py
+++ version_0.5a/regression_data_generator.py
@@ -11,27 +11,19 @@ nlp = spacy.load('en_core_web_sm')
 FUNCTION_WORDS = {'a', 'in', 'of', 'the'}
 
 
-def prepare_data_for_regression(data_file, save_file='data_matrix.csv'):
+def prepare_data_for_regression(data_file, save_file='data_matrix.csv', chunk_size=5):
     """
     This function prepares the data for regression analysis by extracting features and labels from the data.
 
     Args:
     data_file (str): The path to the full_data.csv file.
     save_file (str): The path to the file where the processed data will be saved.
+    chunk_size (int): The number of rows to process at a time.
 
     Returns:
     data_matrix (DataFrame): A DataFrame where each row represents a text, each column represents a feature,
                             and the last column is the label.
     """
-    # Load the data
-    data = pd.read_csv(data_file)
-
-    # Convert the DataFrame to a list of tuples
-    data = list(data.itertuples(index=False, name=None))
-
-    # Initialize lists to store features and labels
-    feature_list = []
-
     # Load the model and tokenizer
     model, tokenizer = load_model()
 
@@ -43,97 +35,111 @@ def prepare_data_for_regression(data_file, save_file='data_matrix.csv'):
         saved_data = pd.DataFrame()
         processed_rows = 0
 
-    # Remove prefixes
-    texts, labels = remove_prefix(data)
-    prompts_and_texts = extract_prompts_and_texts(data)
+    total_rows_processed = processed_rows  # total rows processed in this session
 
-    for i, ((prompt, text), label) in enumerate(zip(prompts_and_texts, labels)):
-        # Skip rows that have already been processed
-        if i < processed_rows:
+    for chunk in pd.read_csv(data_file, chunksize=chunk_size):
+        feature_list = []
+        data = list(chunk.itertuples(index=False, name=None))
+        texts, labels = remove_prefix(data)
+        prompts_and_texts = extract_prompts_and_texts(data)
+
+        for i, ((prompt, text), label) in enumerate(zip(prompts_and_texts, labels)):
+            # Skip rows that have already been processed
+            if total_rows_processed < processed_rows:
+                total_rows_processed += 1
+                continue
+
+            try:
+                # Count POS tags in the text
+                pos_counts, punctuation_counts, function_word_counts = count_pos_tags_and_special_elements(text)
+
+                # Calculate the Flesch Reading Ease and Flesch-Kincaid Grade Level
+                flesch_reading_ease, flesch_kincaid_grade_level = calculate_readability_scores(text)
+
+                # Calculate the average word length
+                avg_word_length = calculate_average_word_length([text])
+
+                # Calculate the average sentence length
+                avg_sentence_length = calculate_average_sentence_length([text])
+
+                # Calculate the perplexity of the text and average sentence perplexity
+                text_encoded = tokenizer.encode(text, truncation=True, max_length=510)
+                text = tokenizer.decode(text_encoded)
+                text = text.replace('<s>', '').replace('</s>', '')
+                text_perplexity = calculate_perplexity(text, model, tokenizer)
+                sentence_perplexities = [calculate_perplexity(sentence.text, model, tokenizer) for sentence in nlp(text).sents]
+                sentence_perplexities = [p for p in sentence_perplexities if p is not None]
+                avg_sentence_perplexity = sum(sentence_perplexities) / len(
+                    sentence_perplexities) if sentence_perplexities else None
+
+                # Calculate the frequency of uppercase letters
+                uppercase_freq = sum(1 for char in text if char.isupper()) / len(text)
+
+                # Calculate the cosine similarity for the prompt and text
+                prompt_text_cosine_similarity = calculate_cosine_similarity(prompt, text, model, tokenizer)
+
+                # Calculate the average cosine similarity for sentences in the text
+                sentence_cosine_similarities = calculate_cosine_similarities_for_sentences_in_text(text, model, tokenizer)
+                avg_sentence_cosine_similarity = None
+                if sentence_cosine_similarities:
+                    avg_sentence_cosine_similarity = sum(sentence_cosine_similarities) / len(sentence_cosine_similarities)
+                else:
+                    print("WARNING: No sentence cosine similarities calculated for text:", text)
+
+                # Prepare a dictionary to append to the feature list
+                features = {
+                    'ADJ': pos_counts.get('ADJ', 0),
+                    'ADV': pos_counts.get('ADV', 0),
+                    'CONJ': pos_counts.get('CONJ', 0),
+                    'NOUN': pos_counts.get('NOUN', 0),
+                    'NUM': pos_counts.get('NUM', 0),
+                    'VERB': pos_counts.get('VERB', 0),
+                    'COMMA': punctuation_counts.get(',', 0),
+                    'FULLSTOP': punctuation_counts.get('.', 0),
+                    'SPECIAL-': punctuation_counts.get('-', 0),
+                    'FUNCTION-A': function_word_counts.get('a', 0),
+                    'FUNCTION-IN': function_word_counts.get('in', 0),
+                    'FUNCTION-OF': function_word_counts.get('of', 0),
+                    'FUNCTION-THE': function_word_counts.get('the', 0),
+                    'uppercase_freq': uppercase_freq,
+                    'flesch_reading_ease': flesch_reading_ease,
+                    'flesch_kincaid_grade_level': flesch_kincaid_grade_level,
+                    'avg_word_length': avg_word_length,
+                    'avg_sentence_length': avg_sentence_length,
+                    'text_perplexity': text_perplexity,
+                    'avg_sentence_perplexity': avg_sentence_perplexity,
+                    'prompt_text_cosine_similarity': prompt_text_cosine_similarity,
+                    'avg_sentence_cosine_similarity': avg_sentence_cosine_similarity,
+                    'label': label
+                }
+
+                # Add the feature dictionary to the feature list
+                feature_list.append(features)
+
+                # Print progress
+                print(f"Processed row {total_rows_processed + 1}")
+                total_rows_processed += 1
+
+            except Exception as e:
+                print(f"Error processing row {total_rows_processed + 1}: {e}")
+                continue
+
+        try:
+            # Convert the list of dictionaries into a DataFrame
+            new_data = pd.DataFrame(feature_list).fillna(0)
+
+            # Append new data to saved data and save
+            saved_data = pd.concat([saved_data, new_data])
+            saved_data.to_csv(save_file, index=False)
+
+            # Clear the feature list for the next batch
+            feature_list.clear()
+
+        except Exception as e:
+            print(f"Error processing chunk: {e}")
             continue
 
-        # Count POS tags in the text
-        pos_counts, punctuation_counts, function_word_counts = count_pos_tags_and_special_elements(text)
-
-        # Calculate the Flesch Reading Ease and Flesch-Kincaid Grade Level
-        flesch_reading_ease, flesch_kincaid_grade_level = calculate_readability_scores(text)
-
-        # Calculate the average word length
-        avg_word_length = calculate_average_word_length([text])
-
-        # Calculate the average sentence length
-        avg_sentence_length = calculate_average_sentence_length([text])
-
-        # Calculate the perplexity of the text and average sentence perplexity
-        # Truncate the text to the first 512 tokens
-        text_encoded = tokenizer.encode(text, truncation=True, max_length=510)
-        text = tokenizer.decode(text_encoded)
-        text = text.replace('<s>', '').replace('</s>', '')
-
-        text_perplexity = calculate_perplexity(text, model, tokenizer)
-        sentence_perplexities = [calculate_perplexity(sentence.text, model, tokenizer) for sentence in nlp(text).sents]
-        sentence_perplexities = [p for p in sentence_perplexities if p is not None]
-        avg_sentence_perplexity = sum(sentence_perplexities) / len(
-            sentence_perplexities) if sentence_perplexities else None
-
-        # Calculate the frequency of uppercase letters
-        uppercase_freq = sum(1 for char in text if char.isupper()) / len(text)
-
-        # Calculate the cosine similarity for the prompt and text
-        prompt_text_cosine_similarity = calculate_cosine_similarity(prompt, text, model, tokenizer)
-
-        # Calculate the average cosine similarity for sentences in the text
-        sentence_cosine_similarities = calculate_cosine_similarities_for_sentences_in_text(text, model, tokenizer)
-        avg_sentence_cosine_similarity = None
-        if sentence_cosine_similarities:
-            avg_sentence_cosine_similarity = sum(sentence_cosine_similarities) / len(sentence_cosine_similarities)
-        else:
-            print("WARNING: No sentence cosine similarities calculated for text:", text)
-
-        # Prepare a dictionary to append to the feature list
-        features = {
-            'ADJ': pos_counts.get('ADJ', 0),
-            'ADV': pos_counts.get('ADV', 0),
-            'CONJ': pos_counts.get('CONJ', 0),
-            'NOUN': pos_counts.get('NOUN', 0),
-            'NUM': pos_counts.get('NUM', 0),
-            'VERB': pos_counts.get('VERB', 0),
-            'COMMA': punctuation_counts.get(',', 0),
-            'FULLSTOP': punctuation_counts.get('.', 0),
-            'SPECIAL-': punctuation_counts.get('-', 0),
-            'FUNCTION-A': function_word_counts.get('a', 0),
-            'FUNCTION-IN': function_word_counts.get('in', 0),
-            'FUNCTION-OF': function_word_counts.get('of', 0),
-            'FUNCTION-THE': function_word_counts.get('the', 0),
-            'uppercase_freq': uppercase_freq,  # new feature
-            'flesch_reading_ease': flesch_reading_ease,
-            'flesch_kincaid_grade_level': flesch_kincaid_grade_level,
-            'avg_word_length': avg_word_length,
-            'avg_sentence_length': avg_sentence_length,
-            'text_perplexity': text_perplexity,
-            'avg_sentence_perplexity': avg_sentence_perplexity,
-            'prompt_text_cosine_similarity': prompt_text_cosine_similarity,  # new feature
-            'avg_sentence_cosine_similarity': avg_sentence_cosine_similarity,  # new feature
-            'label': label
-        }
-
-        # Add the feature dictionary to the feature list
-        feature_list.append(features)
-
-        # Convert the list of dictionaries into a DataFrame
-        new_data = pd.DataFrame(feature_list).fillna(0)
-
-        # Append new data to saved data and save
-        data_matrix = pd.concat([saved_data, new_data])
-        data_matrix.to_csv(save_file, index=False)
-
-        # Clear the feature list for the next batch
-        feature_list.clear()
-
-        # Print progress
-        print(f"Processed row {i + 1} of {len(data)}")
-
-    return data_matrix
+    return saved_data
 
 
 prepare_data_for_regression('extracted_data/full_data.csv')
